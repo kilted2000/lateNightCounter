f1695c25f88a377cc6ccc1acd5765b3cb890d6a6
kilted2000 <53500493+kilted2000@users.noreply.github.com>
2022-11-15 08:15:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let total = 0;
+  
+  let clicked = document.getElementById("btn").addEventListener("click", function counter(){
+    document.getElementById("totalNum").innerHTML = total++;
+    
+  });
+
+
+
+
+
+
+
